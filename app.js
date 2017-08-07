6659d1e1ae180689d2eb8ffbc1c3d538326e4148
Yermek Alzanov <yermek-alzhanov@utulsa.edu>
2017-08-07 15:34:59 -0700

    implement the grid

---
+++ app.js
@@ -0,0 +1,103 @@
+$(function() {
+  var state = true;
+  $('#1').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#1').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#2').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#2').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#3').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#3').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#4').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#4').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#5').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#5').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#6').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#6').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#7').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#7').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#8').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#8').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+  $('#9').on('click', function() {
+    if (state===true) {
+      var mark = 'x'
+    } else {
+      var mark = 'o'
+    }
+    $('#9').text(mark)
+    state=!state
+    console.log($('tbody th'))
+  })
+
+
+});
